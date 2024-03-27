67450855ab2915e7712b1029accb27497ebcea07
brian <brianmurigi19@gmail.com>
2024-03-27 14:52:15 +0300

    done

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -1,18 +1,12 @@
-from versions import (
-    python_version,
-    requests_version,
-    pytest_version
-)
+import sys
+import requests
+import pytest
 
-def test_python_version():
-    version_info = python_version()
-    assert version_info.major == 3
-    assert version_info.minor == 8
+def python_version():
+    return sys.version_info
 
+def requests_version():
+    return requests.__version__
 
-def test_requests_version():
-    assert requests_version() == "2.27.1"
-
-
-def test_pytest_version():
-    assert pytest_version() == "7.1.3"
+def pytest_version():
+    return pytest.__version__
